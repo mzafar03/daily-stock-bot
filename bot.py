49e20b59aaac843a2ac25fe62b5321c43b4c85ba
mzafar03 <md.zafar1993@gmail.com>
2026-02-14 14:19:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # ================= CONFIG =================
 BOT_TOKEN = "8269225957:AAEEWk98nWmLZe5ncUxGf4qRpxFdn7-ENUM"
-CHAT_ID = "123456789"
+CHAT_ID = "1579119023"
 CAPITAL = 100000
 
 stocks = [
